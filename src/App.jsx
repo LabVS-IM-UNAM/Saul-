3a153a6914eb3dea86c8b8bb92edd1de355fb545
Fili <saul.lopez@ext.webedia-group.com>
2025-05-28 23:43:21 -0600

    ADD componente Card y Titulo, integrando bootstrap
    Se elimino los css y se agrego bootstrap. Y se modifico la App para mostrar los componentes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Grid from './components/Grid'
+import * as React from 'react';
+import Card from './components/Card';
 
 function App() {
-    const sampleGrid = Array(30).fill().map(() => Array(30).fill(0));
-    return (
-        <div className="main-container">
-            <h1>Juego de la Vida de Jhon Conway</h1>
-        </div>
-    )
+    return <Card/>;
 }
 
-export default App
+export default App;
---
+++ src/Titulo.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+function Titulo() {
+    const nombre = '';
+    if (nombre){
+        return <h1>Hola {nombre}</h1>
+    }
+    return (
+        <h1>Hola Mundo!</h1>
+    );
+}
+
+export default Titulo;
